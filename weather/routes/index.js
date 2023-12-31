334a1ae462c5f004088ec5f89689ef631cc06905
dillonthompson <dthompson@dj.thompson715@gmail.com>
2023-12-31 00:04:58 -0600

    fuck you

--- weather/routes/index.js
+++ weather/routes/index.js
@@ -13,8 +13,9 @@ router.get('/', function(req, res) {
 
 router.post('/get_weather', async function (req,res) {
   let city = req.body.city;
-  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=${UNITS}&appid=${OWM_API_KEY}`;
+  const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=${UNITS}&appid=${OWM_API_KEY}`;
   console.log(OWM_API_KEY);
+  console.log(url);
 
   try {
     let data = await fetch(url);
